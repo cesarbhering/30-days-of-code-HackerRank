85fa7f8e16b90df5ecf92545d69d926588ca8479
Cesar Bhering <cesar.bhering@ericsson.com>
2020-04-07 11:49:12 -0300

    Desafio 2

--- 02-Operators.py
+++ 02-Operators.py
@@ -1,2 +1,2 @@
 def solve(meal_cost, tip_percent, tax_percent):
-    return print(int(meal_cost + ((meal_cost/100)*tip_percent) + ((meal_cost/100)*tax_percent)))
+    return print(round(meal_cost + ((meal_cost/100)*tip_percent) + ((meal_cost/100)*tax_percent)))
