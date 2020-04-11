16d778948bdc75dcc728033c786bb6b69f050eab
Cesar Bhering <cesar.bhering@ericsson.com>
2020-04-11 11:31:08 -0300

    Desafio 6

---
+++ 06-LetsReview.py
@@ -0,0 +1,8 @@
+for n in range(int(input()))
+  for letra in input():
+       if (teste.index(letra)) % 2 == 0:
+            par += letra
+        else:
+            impar += letra
+
+print(f'{par}  {impar}')
