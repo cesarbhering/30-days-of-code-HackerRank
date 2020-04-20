caa64fa4174c5f3061db8d970746fa95512fa1dc
Cesar Bhering <cesar.bhering@ericsson.com>
2020-04-20 18:32:32 -0300

    Desafio 15

--- 15-LinkedLists.py
+++ 15-LinkedLists.py
@@ -1,10 +1,10 @@
-def insert(self,head,data):
+def insert(self, head, data):
     if head is None:
         head = Node(data)
         self.tail = head
-    else: 
+    else:
         node = Node(data)
         self.tail.next = node
         self.tail = node
     return head
-                
+#TIVE QUE CONSULTA UMA SOLUÇÃO NO FORUM
