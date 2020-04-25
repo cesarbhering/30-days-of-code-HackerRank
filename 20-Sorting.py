22c82825daa0c66df5022a9561b9c5735eac9cbe
Cesar Bhering <cesar.bhering@ericsson.com>
2020-04-25 11:01:39 -0300

    Desafio 20

---
+++ 20-Sorting.py
@@ -0,0 +1,10 @@
+numSwaps = 0
+while a != sorted(a):
+    for i in range(n-1):
+        if a[i] > a[i+1]:
+            a[i], a[i+1] = a[i+1], a[i]
+            numSwaps +=1
+
+print(f'Array is sorted in {numSwaps} swaps.')
+print(f'First Element: {sorted(a)[0]}')
+print(f'Last Element: {sorted(a)[-1]}')
